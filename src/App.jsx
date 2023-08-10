b2b62b23f44555e3713bacf37c5fffbd598e630b
the-soban <sobanshafeq123@gmail.com>
2023-08-10 20:52:09 +0500

    practiced basic usage of react query using useQuery object

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css'
+import { useQuery, useMutation } from '@tanstack/react-query'
+
+const POSTS = [
+    { id: 1, title: 'Post 1' },
+    { id: 2, title: 'Post 2' },
+]
 
 function App() {
+    const postsQuery = useQuery({
+        queryKey: ['posts'],
+        queryFn: () => Promise.reject('I reject it!!'),
+    })
 
-  return (
-    <>
-    <h1>Hello World</h1>
-    </>
-  )
+    if (postsQuery.isLoading) {
+        return <h1>Loading...</h1>
+    }
+    if (postsQuery.isError) {
+        return <h3>{JSON.stringify(postsQuery.error)}</h3>
+    }
+
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+
+const wait = (duration) => {
+    return new Promise((resolve) => setTimeout(resolve, duration))
 }
 
 export default App
