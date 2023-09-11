90529980d8571c1fa3510e756e848af6b3055ff4
the-soban <sobanshafeq123@gmail.com>
2023-09-11 22:35:25 +0500

    practiced basic functionality of react query: queryKeys and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
-import './App.css'
-import { useQuery, useMutation } from '@tanstack/react-query'
+import React from 'react';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import './App.css';
 
 const POSTS = [
     { id: 1, title: 'Post 1' },
     { id: 2, title: 'Post 2' },
-]
+];
 
-function App() {
+const App = () => {
     const postsQuery = useQuery({
-        queryKey: ['posts'], //first thing to give to the query: a unique identifier key
-        queryFn: () => wait(1000).then(() => [...POSTS]), //second thing: a function that fetches the actual data, could be an axios request too, or a fetch method call
-    })
-
-    const newMutation = useMutation({
-      mutationFn
-    })
+        queryKey: ['posts'], //this is a unique identifier for this query, and it always accepts an array as a value
+        queryFn: () => wait(1000).then(() => [...POSTS]), //this is the actual query function that'll be performed, and it always accepts a promise
+        // queryFn: () => Promise.reject('This is error message'), //this message will be returned if there's error
+    });
 
     if (postsQuery.isLoading) {
-        return <h1>Loading...</h1>
+        return <h2>Loading, please wait...</h2>;
     }
     if (postsQuery.isError) {
-        return <h3>{JSON.stringify(postsQuery.error)}</h3>
+        return <pre>{JSON.stringify(postsQuery.error)}</pre>; //shows an error message if there's an error
     }
 
     return (
         <>
             <h1>Hello World</h1>
-            {postsQuery.data.map((post) => (
-                <div key={post.id}>{post.title}</div> //to loop through the data show each of our post
-            ))}
         </>
-    )
-}
+    );
+};
 
 const wait = (duration) => {
-    return new Promise((resolve) => setTimeout(resolve, duration))
-}
+    return new Promise((resolve) => setTimeout(resolve, duration));
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+//first thing to do after installing react query is to wrap the whole app with a QueryClient provider:
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
