93d9038b92436dae7242a29330ddf166b8b1e2f2
the-soban <sobanshafeq123@gmail.com>
2023-09-13 00:04:48 +0500

    imported React query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 //first thing to do after installing react query is to wrap the whole app with a QueryClient provider:
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <App />
+            <ReactQueryDevtools />
         </QueryClientProvider>
     </React.StrictMode>
-)
+);
